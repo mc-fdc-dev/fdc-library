28c6b4582e064c237b73f41b00d0a941bf01540c
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-08 06:16:37 -1000

    Create sql.py

---
+++ fdc_library/sql.py
@@ -0,0 +1,63 @@
+from aiomysql import Pool
+
+from typing import Any
+
+
+class BaseColumn:
+    name: str | None = None
+    type: Any = None
+        
+class TextColumn(BaseColumn):
+    name = "TEXT"
+    type = str
+
+class BigintColumn(BaseColumn):
+    name = "BIGINT"
+    type = int
+
+class Table:
+    name: str
+    columns: dict
+    pool: Pool
+    def __init_subclass__(cls, *, name: str | None = None):
+        cls.name = cls.__name__ if name is None else name
+        cls.columns = {}
+        for name in dir(cls):
+            if isinstance(column := getattr(cls, name), BaseColumn):
+                cls.columns[name] = getattr(cls, name)
+
+    async def create(self, pool: Pool) -> None:
+        async with pool.acquire() as connection:
+            async with connection.cursor() as cursor:
+                await cursor.execute("CREATE TABLE IF NOT EXISTS {} ({});".format(
+                    self.name, ", ".join(
+                        f"{name} {column.name}" for name, column in self.columns.items()
+                )))
+
+    async def insert(self, **kwargs):
+        for name, value in kwargs.items():
+            if name not in self.columns:
+                raise KeyError(f"{name} is not a column in {self.name}")
+            if not isinstance(value, self.columns[name].type):
+                raise TypeError(f"{name} is not of type {self.columns[name].type}")
+        async with self.pool.acquire() as connection:
+            async with connection.cursor() as cursor:
+                await cursor.execute("INSERT INTO {} ({}) VALUES ({});".format(
+                    self.name, ", ".join(kwargs.keys()), ", ".join("%s" for _ in kwargs.values())
+                ), (*kwargs.values(),))
+
+    async def select_all(self, *, mode: str="one"):
+        async with self.pool.acquire() as connection:
+            async with connection.cursor() as cursor:
+                await cursor.execute("SELECT * FROM {};".format(self.name))
+                if mode == "one":
+                    return await cursor.fetchone()
+                elif mode == "all":
+                    return await cursor.fetchall()
+
+    async def select(self, *, mode: str="one", **kwargs):
+        async with self.pool.acquire() as connection:
+            async with connection.cursor() as cursor:
+                await cursor.execute("SELECT * FROM {} WHERE{};".format(
+                    self.name, "AND ".join("
+                ))
